37a8440ccc2994c42acce716c077d1146d68d3f3
ubuntu <ubuntu@ubuntu-VirtualBox.(none)>
2016-03-01 12:13:32 +0100

    td4

--- Thread.h
+++ Thread.h
@@ -1,2 +1,19 @@
+#ifndef THREAD_H
+#define THREAD_H
+
 #include <pthread.h>
 
+class Thread
+{
+public:
+	void Thread(int schedPolicy);
+	void start(int priority);
+	void join();
+protected:
+	virtual void run();
+private:
+	pthread_t tid;
+	int schedPolicy;
+};
+
+#endif
