ffee8bb6b48f737eac417c8f696ea73e716a77af
antoineb19 <antoinebenoit@msn.com>
2016-03-28 17:42:20 +0200

    semaphore debug

--- TD4D/mainExoD.cpp
+++ TD4D/mainExoD.cpp
@@ -1,6 +1,27 @@
 #include "SemaphoreCounterThread.h"
 #include "Semaphore.h"
 
+
+void printTime(timespec *t2, timespec *t1)
+{
+    /**
+     * Print la différence entre t1 et t2
+     */
+    float sec = t1->tv_sec - t2->tv_sec;
+    float nsec = t1->tv_nsec - t2->tv_nsec;
+
+    if(nsec < 0)  // Pour gérer le cas où le temps d'exécution serait supérieur à 1s
+    {
+        sec--;
+        nsec = 1000000000-nsec;
+    }
+
+    float nsecDiff = fmod(nsec, 1000000000);
+    float secDiff = (nsec - nsecDiff) / 1000000000 + sec;
+
+    printf("Le temps total d'exécution est de : %.0f,%.0fs\n\n", secDiff, nsecDiff);
+}
+
 int main(int argc, char *argv[]) {
     int schedPolicy = SCHED_RR;
     double counter = 0.0;
@@ -32,24 +53,4 @@ int main(int argc, char *argv[]) {
 
     printf("compteur : %0.f\n", counter);
     return 0;
-}
-
-void printTime(timespec *t2, timespec *t1)
-{
-    /**
-     * Print la différence entre t1 et t2
-     */
-    float sec = t1->tv_sec - t2->tv_sec;
-    float nsec = t1->tv_nsec - t2->tv_nsec;
-
-    if(nsec < 0)  // Pour gérer le cas où le temps d'exécution serait supérieur à 1s
-    {
-        sec--;
-        nsec = 1000000000-nsec;
-    }
-
-    float nsecDiff = fmod(nsec, 1000000000);
-    float secDiff = (nsec - nsecDiff) / 1000000000 + sec;
-
-    printf("Le temps total d'exécution est de : %.0f,%.0fs\n\n", secDiff, nsecDiff);
 }
