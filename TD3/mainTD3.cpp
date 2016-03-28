b434c61b20a939e89d682f590af194348761aa36
Maxime Piard <maxime.piard@student.ecp.fr>
2016-03-29 00:08:27 +0200

    wip

--- TD3/mainTD3.cpp
+++ TD3/mainTD3.cpp
@@ -1,8 +1,9 @@
 #include "Chrono.h"
 #include <stdio.h>
+#include <windows.h>
 int main(int argc, char *argv[]){
     Chrono chrono1 = Chrono();
-    //Sleep ?
+    Sleep(30);
     chrono1.stop();
     Timespec temps;
     printf("lolfd %f\n", chrono1.lap());
