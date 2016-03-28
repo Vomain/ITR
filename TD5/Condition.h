7a5e8b75b8325982b37886643a3e5816b68a6300
antoineb19 <antoinebenoit@msn.com>
2016-03-28 20:39:35 +0200

    td5 init

---
+++ TD5/Condition.h
@@ -0,0 +1,26 @@
+#ifndef CONDITION_H_INCLUDED
+#define CONDITION_H_INCLUDED
+
+#include "Mutex.h"
+
+class Condition : public Mutex {
+public:
+    Condition();
+
+    Condition(bool);
+
+    void wait();
+
+    bool wait(double timeout_ms);
+
+    void notify();
+
+    void notifyAll();
+
+    ~Condition();
+
+private:
+    pthread_cond_t cid;
+};
+
+#endif
---
+++ TD5/mainExoD.cpp
@@ -0,0 +1,43 @@
+#include "SemaphoreCounterThread.h"
+#include "Semaphore.h"
+
+
+double semaphoreCount(unsigned nTokens, unsigned count)
+{
+    int schedPolicy = SCHED_RR;
+    double counter = 0.0;
+
+    Semaphore semaphore(nTokens, nTokens);
+    SemaphoreCounterThread threadA(schedPolicy, count, &counter, &semaphore);
+    SemaphoreCounterThread threadB(schedPolicy, count, &counter, &semaphore);
+    SemaphoreCounterThread threadC(schedPolicy, count, &counter, &semaphore);
+    
+    
+    Timespec t1, t2;
+    clock_gettime(CLOCK_REALTIME, &t1);
+    
+    threadA.start(8);
+    threadB.start(8);
+    threadC.start(8);
+    
+    threadA.join(8);
+    threadB.join(8);
+    threadC.join(8);
+
+    clock_gettime(CLOCK_REALTIME, &t2);   
+    t2 = t2 - t1;
+    return t2.to_ms();
+}
+
+int main(int argc, char *argv[]) {
+    for(int nToken = 1; nToken < 4; nToken ++){
+        printf("NTOKEN = %d\n", nToken);
+        double mean = 0.0;
+        for(int i = 0 ; i < 5; i++){
+            double time = semaphoreCount(nToken,200);
+            printf("        time : %f\n", time);
+            mean += time / 5;
+        }
+        printf("        Mean time for nToken=%d : %f\n", nToken,mean);
+    }
+}
