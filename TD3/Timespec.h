328e3a4cae03cfbf32aa7f6841a8cc0ce4c9f39f
Maxime Piard <maxime.piard@student.ecp.fr>
2016-03-28 22:21:23 +0200

    fork - only timespec class

---
+++ TD3/Timespec.h
@@ -0,0 +1,22 @@
+#ifndef ITR_TIMESPEC_H
+#define ITR_TIMESPEC_H
+
+#include <time.h>
+#include <math.h>
+
+class Timespec : public timespec { // Hérite de timespec
+public:
+    Timespec();
+
+    Timespec from_ms(double);
+
+    double to_ms();
+};
+
+Timespec operator+(Timespec, Timespec);
+
+Timespec operator-(Timespec, Timespec);
+
+bool operator<(Timespec, Timespec);
+
+#endif //ITR_TIMESPEC_H
