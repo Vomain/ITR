4043acabc9a94cd01912a4d7fd2ed995182a07da
antoineb19 <antoinebenoit@msn.com>
2016-03-28 16:12:01 +0200

    sempahore init

--- TD4D/Semaphore.cpp
+++ TD4D/Semaphore.cpp
@@ -3,7 +3,7 @@
 
 unsigned UINT_MAX = 1000;
 
-Semaphore::Semaphore(unsigned counter = 0, unsigned maxCount = UINT_MAX) : counter(counter), maxCount(maxCount), condition()
+Semaphore::Semaphore(unsigned counter = 0, unsigned maxCount = UINT_MAX) : counter(counter), maxCount(maxCount), condition(new Condition())
 {}
 
 
