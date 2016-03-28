db93b0d3726fe8a74ccb138adeea1d13f666f29d
antoineb19 <antoinebenoit@msn.com>
2016-03-28 16:34:14 +0200

    semaphore debug

--- TD4D/SemaphoreCounterThread.cpp
+++ TD4D/SemaphoreCounterThread.cpp
@@ -8,7 +8,7 @@ void SemaphoreCounterThread::run() {
     for (int i = 0; i < nLoops; i++) {
         semaphore->take();
         *pCounter += 1.0;
-        sleep(100);
+        sleep(1000);
         semaphore->give();
     }
 }
