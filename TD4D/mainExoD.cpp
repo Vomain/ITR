45dbc74df60f100eccc52dbd4b7c30e3be66d397
antoineb19 <antoinebenoit@msn.com>
2016-03-28 17:36:04 +0200

    semaphore debug

--- TD4D/mainExoD.cpp
+++ TD4D/mainExoD.cpp
@@ -28,10 +28,28 @@ int main(int argc, char *argv[]) {
     Timespec t2;
     clock_gettime(CLOCK_REALTIME, &t2);    
     
-    t2 = t2 - t1;
-    
-    printf("countThreads done! %f:%f\n", t2.tv_sec, t2.tv_nsec);
+    printTime(&t1, &t2);
 
     printf("compteur : %0.f\n", counter);
     return 0;
+}
+
+void printTime(timespec *t2, timespec *t1)
+{
+    /**
+     * Print la différence entre t1 et t2
+     */
+    float sec = t1->tv_sec - t2->tv_sec;
+    float nsec = t1->tv_nsec - t2->tv_nsec;
+
+    if(nsec < 0)  // Pour gérer le cas où le temps d'exécution serait supérieur à 1s
+    {
+        sec--;
+        nsec = 1000000000-nsec;
+    }
+
+    float nsecDiff = fmod(nsec, 1000000000);
+    float secDiff = (nsec - nsecDiff) / 1000000000 + sec;
+
+    printf("Le temps total d'exécution est de : %.0f,%.0fs\n\n", secDiff, nsecDiff);
 }
