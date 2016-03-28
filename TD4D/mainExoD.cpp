ffe401139d43a62071779d11ee98b88264531352
antoineb19 <antoinebenoit@msn.com>
2016-03-28 18:17:11 +0200

    semaphore debug

--- TD4D/mainExoD.cpp
+++ TD4D/mainExoD.cpp
@@ -1,28 +1,7 @@
 #include "SemaphoreCounterThread.h"
 #include "Semaphore.h"
 
-
-void printTime(timespec *t2, timespec *t1)
-{
-    /**
-     * Print la différence entre t1 et t2
-     */
-    float sec = t1->tv_sec - t2->tv_sec;
-    float nsec = t1->tv_nsec - t2->tv_nsec;
-
-    if(nsec < 0)  // Pour gérer le cas où le temps d'exécution serait supérieur à 1s
-    {
-        sec--;
-        nsec = 1000000000-nsec;
-    }
-
-    float nsecDiff = fmod(nsec, 1000000000);
-    float secDiff = (nsec - nsecDiff) / 1000000000 + sec;
-
-    printf("Le temps total d'exécution est de : %.0f,%.0fs\n\n", secDiff, nsecDiff);
-}
-
-int semaphoreCount(unsigned nTokens, unsigned count)
+double semaphoreCount(unsigned nTokens, unsigned count)
 {
     int schedPolicy = SCHED_RR;
     double counter = 0.0;
@@ -33,8 +12,7 @@ int semaphoreCount(unsigned nTokens, unsigned count)
     SemaphoreCounterThread threadC(schedPolicy, count, &counter, &semaphore);
     SemaphoreCounterThread threadD(schedPolicy, count, &counter, &semaphore);
     
-    struct timespec t1;
-	struct timespec t2;
+    Timespec t1, t2
     clock_gettime(CLOCK_REALTIME, &t1);
     
     threadA.start(8);
@@ -48,17 +26,19 @@ int semaphoreCount(unsigned nTokens, unsigned count)
     threadD.join(8);
 
     clock_gettime(CLOCK_REALTIME, &t2);   
-    
-    printf("nTokens : %d, count : %d\n", nTokens, count);
-    printTime(&t1, &t2);
-    printf("compteur : %0.f\n", counter);
-    
-    return 0;
+    t2 = t1 - t2;
+    return t2.to_ms();
 }
 
 int main(int argc, char *argv[]) {
-    semaphoreCount(1,1000);
-    semaphoreCount(2,1000);
-    semaphoreCount(3,1000);
-    semaphoreCount(4,1000);
+    for(int nToken = 1; nToken < 5; nToken ++){
+        printf("NTOKEN = %d", nToken);
+        double mean = 0.0;
+        for(int i = 0 ; i < 5; i++){
+            double time = semaphoreCount(1,1000);
+            printf("        time : %f", time);
+            mean += time / 10;
+        }
+        printf("        Mean time for nToken=%d : %f", nToken,mean);
+    }
 }
