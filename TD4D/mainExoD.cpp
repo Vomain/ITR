1c36fde031f205480cba4721655d5330d8106a31
antoineb19 <antoinebenoit@msn.com>
2016-03-28 18:24:21 +0200

    semaphore debug

--- TD4D/mainExoD.cpp
+++ TD4D/mainExoD.cpp
@@ -12,6 +12,11 @@ double semaphoreCount(unsigned nTokens, unsigned count)
     SemaphoreCounterThread threadB(schedPolicy, count, &counter, &semaphore);
     SemaphoreCounterThread threadC(schedPolicy, count, &counter, &semaphore);
     SemaphoreCounterThread threadD(schedPolicy, count, &counter, &semaphore);
+    SemaphoreCounterThread threadE(schedPolicy, count, &counter, &semaphore);
+    SemaphoreCounterThread threadF(schedPolicy, count, &counter, &semaphore);
+    SemaphoreCounterThread threadG(schedPolicy, count, &counter, &semaphore);
+    SemaphoreCounterThread threadH(schedPolicy, count, &counter, &semaphore);
+    
     
     Timespec t1, t2;
     clock_gettime(CLOCK_REALTIME, &t1);
@@ -20,11 +25,19 @@ double semaphoreCount(unsigned nTokens, unsigned count)
     threadB.start(8);
     threadC.start(8);
     threadD.start(8);
+    threadE.start(8);
+    threadF.start(8);
+    threadG.start(8);
+    threadH.start(8);
     
     threadA.join(8);
     threadB.join(8);
     threadC.join(8);
     threadD.join(8);
+    threadE.join(8);
+    threadF.join(8);
+    threadG.join(8);
+    threadH.join(8);
 
     clock_gettime(CLOCK_REALTIME, &t2);   
     t2 = t2 - t1;
@@ -32,11 +45,11 @@ double semaphoreCount(unsigned nTokens, unsigned count)
 }
 
 int main(int argc, char *argv[]) {
-    for(int nToken = 1; nToken < 5; nToken ++){
+    for(int nToken = 1; nToken < 9; nToken ++){
         printf("NTOKEN = %d\n", nToken);
         double mean = 0.0;
         for(int i = 0 ; i < 5; i++){
-            double time = semaphoreCount(nToken,1000);
+            double time = semaphoreCount(nToken,200);
             printf("        time : %f\n", time);
             mean += time / 5;
         }
