b94502dfa592b1db8be7012bed23b846e7dc7758
antoineb19 <antoinebenoit@msn.com>
2016-03-29 01:55:16 +0200

    td5 bug

--- TD4/mainExoE.cpp
+++ TD4/mainExoE.cpp
@@ -3,31 +3,28 @@
 #include "Producer.h"
 
 int main(int argc, char *argv[]) {
-    printf("\nEXERCIwdbvqsCE 5\n");
 
     int schedPolicy = SCHED_RR;
-    printf("Start  !");
 
     Fifo<int> fifo;  // stack of ints
-    printf("Start  !");
 
-    Producer producer1(schedPolicy, 1000, &fifo);
+    Producer producer1(schedPolicy, 2000, &fifo);
     Producer producer2(schedPolicy, 2000, &fifo);
     Consumer consumer1(schedPolicy, 1000, &fifo);
     Consumer consumer2(schedPolicy, 1000, &fifo);
     Consumer consumer3(schedPolicy, 1000, &fifo);
-    printf("Start  !");
+
     producer1.start(8);
     producer2.start(8);
     consumer1.start(8);
     consumer2.start(8);
     consumer3.start(8);
-    printf("Join !");
-    producer1.join(1000.0);
-    producer2.join(1000.0);
-    consumer1.join(1000.0);
-    consumer2.join(1000.0);
-    consumer3.join(1000.0);
+
+    producer1.join();
+    producer2.join();
+    consumer1.join();
+    consumer2.join();
+    consumer3.join();
     
     printf("Terminé !");
     printf("Compteurs : \n   1 : %d, \n   2 : %d, \n   3 : %d\n", consumer1.compteur, consumer2.compteur, consumer3.compteur);
