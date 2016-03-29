22b4f8027ae3e275fe77f65bc0ae17d092fc05ec
antoineb19 <antoinebenoit@msn.com>
2016-03-29 02:14:28 +0200

    td5 bug try

--- TD4E/Condition.cpp
+++ TD4E/Condition.cpp
@@ -1,9 +1,14 @@
+/** @file */
+
 #include "Condition.h"
 #include <stdio.h>
 #include "Timespec.h"
-#include "errno.h"
+#include <stdexcept>
 
-Condition::Condition() : Mutex(true) { }
+
+Condition::Condition() : Mutex(false) {
+    pthread_cond_init(&cid, NULL);
+}
 
 Condition::Condition(bool isInversionSafe)
         : Mutex(isInversionSafe) {
@@ -12,6 +17,10 @@ Condition::Condition(bool isInversionSafe)
 
 void Condition::wait() {
     int error = pthread_cond_wait(&cid, &mid);
+    if(error == 1)
+    {
+        throw std::logic_error( "Le mutex de la condition n'est pas détenu par le thread courrant" );
+    }
 }
 
 bool Condition::wait(double timeout_ms) {
